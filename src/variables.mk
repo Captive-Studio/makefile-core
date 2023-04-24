913168e19b36ea7f976788ba99c15af8d2011047
Julien Polo <julien.polo@captive.fr>
2023-04-24 23:37:44 +0200

    ♻️ Use include.mk as main include

--- src/variables.mk
+++ src/variables.mk
@@ -3,7 +3,7 @@ MAKEFILE_PREFIX := ./makefiles
 # URL to the updater script
 MAKEFILE_CORE := $(MAKEFILE_PREFIX)/core.mk
 # URL to the updater script
-MAKEFILE_UPDATER_URL := https://raw.githubusercontent.com/Captive-Studio/makefile-core/main/module.mk
+MAKEFILE_UPDATER_URL := https://raw.githubusercontent.com/Captive-Studio/makefile-core/main/include.mk
 ## Makefiles to be included (default "makefiles/*/Makefile", "makefiles/*/*.{mk,make}")
 MAKEFILE_INCLUDE ?= $(wildcard $(MAKEFILE_PREFIX)/*/Makefile) $(wildcard $(MAKEFILE_PREFIX)/*/*.make) $(wildcard $(MAKEFILE_PREFIX)/*/*.mk)
 ## Makefiles to be excluded (default "makefiles/_kernel.mk")
