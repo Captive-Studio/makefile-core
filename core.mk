8e6768d7f567773233852bdeec02521679230bd1
Julien Polo <julien.polo@captive.fr>
2023-04-25 10:27:30 +0200

    🐛 Makefile.local should be put at the end

--- core.mk
+++ core.mk
@@ -105,12 +105,13 @@ endif
 #⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯
 
 #
-# Include Makefile.local if it exists
-#
--include $(MAKEFILE_LOCAL)
-
-#
 # Includes every makefiles configured by MAKEFILE_INCLUDE excluding MAKEFILE_EXCLUDE
 #
 include $(filter-out $(MAKEFILE_EXCLUDE),$(MAKEFILE_INCLUDE))
+
+#
+# Include Makefile.local if it exists
+#
+# /!\ WARNING : this must be at the end of all files
+-include $(MAKEFILE_LOCAL)
 
