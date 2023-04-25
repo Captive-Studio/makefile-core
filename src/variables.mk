aa0788eea1ceeb6d4cb96c8904e19888ffc0dce2
Julien Polo <julien.polo@captive.fr>
2023-04-25 22:17:59 +0200

    🐛 Fix misc flags

--- src/variables.mk
+++ src/variables.mk
@@ -1,8 +1,15 @@
-# Do not:
-# -  use make's built-in rules and variables
-#    (this increases performance and avoids hard-to-debug behaviour);
-# -  print "Entering directory ...";
-MAKEFLAGS += -rR --no-print-directory
+# Disable built-in rules and variables
+# (this increases performance and avoids hard-to-debug behaviour);
+MAKEFLAGS += -rR
+
+# Avoid messages "Entering directory ..."
+MAKEFLAGS += --no-print-directory
+
+# Avoid funny character set dependencies
+unexport LC_ALL
+LC_COLLATE=C
+LC_NUMERIC=C
+export LC_COLLATE LC_NUMERIC
 
 # Directory containing all makefiles
 MAKEFILE_PREFIX := ./makefiles
