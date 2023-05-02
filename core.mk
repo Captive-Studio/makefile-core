27336baa783fbbcf22d8e9b393a3089a6c9c4f21
Julien Polo <julien.polo@captive.fr>
2023-05-02 14:08:15 +0200

    ✨ Add color variables

--- core.mk
+++ core.mk
@@ -8,6 +8,24 @@
 #⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯
 
 #⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯
+# CONSOLE
+# @see src/console.mk
+#⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯
+
+# Terminal colors
+BLUE      := $(shell tput -Txterm setaf 4)
+GREEN     := $(shell tput -Txterm setaf 2)
+TURQUOISE := $(shell tput -Txterm setaf 6)
+WHITE     := $(shell tput -Txterm setaf 7)
+YELLOW    := $(shell tput -Txterm setaf 3)
+GREY      := $(shell tput -Txterm setaf 1)
+RESET     := $(shell tput -Txterm sgr0)
+RED       := $(shell tput -Txterm setaf 1)
+
+SMUL      := $(shell tput smul)
+RMUL      := $(shell tput rmul)
+
+#⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯
 # VARIABLES
 # @see src/variables.mk
 #⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯
@@ -91,8 +109,8 @@ override _self_add_module = $(or $(name), $(notdir $(url)), '')
 #
 # Example : make self-add url=https://github.com/ianstormtaylor/makefile-help
 #
-## url=<url> [name=<string>] Add a makefile module (as git submodule)
 .PHONY: self-add
+## url=<url> [name=<string>] Add a makefile module (as git submodule)
 self-add: .gitmodules $(MAKEFILE_PREFIX)
 	@$(GIT) submodule add \
 		--force \
@@ -107,8 +125,8 @@ self-add: .gitmodules $(MAKEFILE_PREFIX)
 #
 # Example : make self-update
 #
-## Update all makefile modules
 .PHONY: self-update
+## Update all makefile modules
 self-update: $(MAKEFILE_PREFIX)
 ifdef update
 # Actual update
---
+++ src/console.mk
@@ -0,0 +1,12 @@
+# Terminal colors
+BLUE      := $(shell tput -Txterm setaf 4)
+GREEN     := $(shell tput -Txterm setaf 2)
+TURQUOISE := $(shell tput -Txterm setaf 6)
+WHITE     := $(shell tput -Txterm setaf 7)
+YELLOW    := $(shell tput -Txterm setaf 3)
+GREY      := $(shell tput -Txterm setaf 1)
+RESET     := $(shell tput -Txterm sgr0)
+RED       := $(shell tput -Txterm setaf 1)
+
+SMUL      := $(shell tput smul)
+RMUL      := $(shell tput rmul)
