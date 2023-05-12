0d60d39dc60b3410b736f5b41c546351d6a66635
Julien Polo <julien.polo@captive.fr>
2023-05-12 18:05:04 +0200

    ✨ Add installer

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Stop when errors
+set -e
+
+MAKEFILES_PREFIX="makefiles"
+MAKEFILECORE_FILE="$MAKEFILES_PREFIX/core.mk"
+INCLUDE_TEMPLATE="include $MAKEFILECORE_FILE"
+
+# Download core module
+mkdir -p $MAKEFILES_PREFIX
+curl -fsSL -o "$MAKEFILECORE_FILE" https://raw.githubusercontent.com/Captive-Studio/makefile-core/main/core.mk
+
+# Update Makefile
+touch Makefile
+if ! grep "$INCLUDE_TEMPLATE" Makefile; then
+  echo -e "$INCLUDE_TEMPLATE\n\n$(cat Makefile)" > Makefile
+fi
