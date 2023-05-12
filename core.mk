106e58e8a09966f8d30d75a419064009d6bb6b13
Julien Polo <julien.polo@captive.fr>
2023-05-12 16:56:14 +0200

    ♻️ Remove unnecessary escaping

--- core.mk
+++ core.mk
@@ -186,7 +186,7 @@ help: ## Show this help.
 	@$(SED) \
 		-e '/^[a-zA-Z0-9_\-]*:.*##/!d' \
 		-e 's/:.*##[[:space:]]*/|/' \
-		-e 's/\(.*\)\|\(.*\)/$(HELP_COLOR_TARGETS)\1$(HELP_COLOR_RESET)|\2/' \
+		-e 's/\(.*\)|\(.*\)/$(HELP_COLOR_TARGETS)\1$(HELP_COLOR_RESET)|\2/' \
 		-e 's/\(.*\)/    \1/' \
 		$(MAKEFILE_LIST) | column -c2 -t -s '|' | sort
 	@echo ''
