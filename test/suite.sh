a076c9f7033a5467087c6962d01a841405f7f807
Julien Polo <julien.polo@captive.fr>
2023-09-06 16:18:43 +0200

    ✅ Refactor les tests

--- test/suite.sh
+++ test/suite.sh
@@ -3,8 +3,10 @@ _TERM=$TERM
 
 unset TERM;
 assert "$MAKE_TEST test-included" 'test-included-done'
-assert_raises "$MAKE_TEST self-update" 0
 
+assert_snapshot "$MAKE_TEST print-variables" "make_print-variables.out"
 assert_snapshot "$MAKE_TEST help" "make_help.out"
-assert_snapshot "$MAKE_TEST" "make_help.out"
+assert_snapshot "$MAKE_TEST" "make_default_help.out"
+
+# assert_raises "$MAKE_TEST self-update" 0
 assert_end
