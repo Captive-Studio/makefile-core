201dafbe1559765e75b46a46523d0ccbdb7641eb
Julien Polo <julien.polo@captive.fr>
2025-02-06 15:27:50 +0100

    ✨ Add lowercase function

--- src/functions.mk
+++ src/functions.mk
@@ -1,9 +1,28 @@
 # Return a newline character
 #
-# @example
+# Example:
 #   $(newline)
 define newline
 
 
 endef
+
+# Escape a shell string passed as a single quoted string
+#
+# Usage:
+# 	$(call escape-shell,<string>)
+#
+# Example:
+#  embeddable-text = $(call escape-shell,$(SOME_TEXT))
+#
+escape-shell = $(subst $(newline),\$(newline),$(subst ','\'',$(1)))
+
+# Lower-case a string value.
+#
+# Usage:
+# 	$(call lowercase,<string>)
+#
+# Example:
+# 	$(call lowercase,HeLlO wOrLd) # "hello world"
+lowercase = $(shell echo $(call escape-shell,$(1)) | tr '[:upper:]' '[:lower:]')
 
